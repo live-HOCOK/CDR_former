5a3abd261f3f70f4e89e071b7e1e0fc89403522c
live-HOCOK <zaordy@gmail.com>
2020-02-06 03:31:00 +0300

    Изменена форма добавлен метод обработки ошибок ввода добавлен метод конвертации введенных данных

---
+++ src/main/converter/converter.kt
@@ -0,0 +1,12 @@
+package main.converter
+
+import javafx.scene.control.Label
+
+
+class converter() {
+
+    fun convert (unparsedData: Array<String>, label: Label) {
+
+    }
+
+}
--- src/main/gui/Controller.java
+++ src/main/gui/Controller.java
@@ -1,20 +1,47 @@
 package main.gui;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import main.converter.converter;
+import java.time.format.DateTimeFormatter;
 
-import java.awt.*;
 
 public class Controller {
 
 
     public TextField textMsisdn;
     public Label welcomeLabel;
+    public TextField textMsisdnB;
+    public DatePicker dateStarDate;
+    public TextField textStartTime;
+    public TextField textDuration;
+    public CheckBox chPlusMinute;
+    public CheckBox chUrgency;
+
 
     public void buttonClick(ActionEvent actionEvent) {
-        welcomeLabel.setText(textMsisdn.getText());
+
+        if (checkField()) {
+            String[] unparsedData = {textMsisdn.getText(),
+                    textMsisdnB.getText(),
+                    dateStarDate.getValue().format(DateTimeFormatter.ofPattern("dd.MM.YYYY")),
+                    textStartTime.getText(),
+                    textDuration.getText(),
+                    String.valueOf(chPlusMinute.isSelected()),
+                    String.valueOf(chUrgency.isSelected())};
+
+            converter convert = new converter();
+            convert.convert(unparsedData, welcomeLabel);
+        }
+
     }
 
+    private boolean checkField() {
+
+        return true;
+    }
 
 }

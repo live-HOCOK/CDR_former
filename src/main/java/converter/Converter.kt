774f3dff230816c7e5e97ac6359605ce08d06c0d
Ilya <ilnikride@gmail.com>
2020-02-21 23:32:07 +0400

    added test for converter

--- src/main/java/converter/Converter.kt
+++ src/main/java/converter/Converter.kt
@@ -5,8 +5,26 @@ import javafx.scene.control.Label
 
 class Converter() {
 
-    fun convert (unparsedData: Array<String>, label: Label) {
-        label.text = "asasd"
+    fun convert(unparsedData: Array<String>, label: Label) {
+        label.text = "asasd"//?
+
     }
+
+    fun convertString(unparsedData: Array<String>): String {
+        var string = ""
+
+        for (s in unparsedData) {
+            var newS = ""
+            if (unparsedData.lastIndex != unparsedData.indexOf(s)) {
+                newS = s.plus(",")
+            } else {
+                string = string.plus(s)
+            }
+
+            string = string.plus(newS)
+        }
+        return string
+    }
+
 
 }

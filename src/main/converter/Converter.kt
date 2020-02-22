9dc7b36b1afb0f64f369ca58ed0a54b0a9a8e1f6
live-HOCOK <zaordy@gmail.com>
2020-02-22 20:26:01 +0300

    Convert to csv * Добавлен метод конвертации * добавлен метод конвертации boolean значений

--- src/main/converter/Converter.kt
+++ src/main/converter/Converter.kt
@@ -6,7 +6,22 @@ import javafx.scene.control.Label
 class Converter() {
 
     fun convert (unparsedData: Array<String>, label: Label) {
-        label.text = "asasd"
+        var parsedData: String
+
+        var msisdn: String = unparsedData[0]
+        var msisdnB: String = unparsedData[1]
+        var startDate: String = unparsedData[2]
+        var startTime: String = unparsedData[3]
+        var duration: String = unparsedData[4]
+        var plusMinute: String = convertBoolean(unparsedData[5])
+        var urgency: String = convertBoolean(unparsedData[6])
+
+        parsedData = "$msisdn,$msisdnB,$startDate,$startTime,$duration,$plusMinute,$urgency"
+        label.text = parsedData
+    }
+
+    private fun convertBoolean(bolString: String): String{
+        return if (bolString.equals("true")) "1" else "0"
     }
 
 }

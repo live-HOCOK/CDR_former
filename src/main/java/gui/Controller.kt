0889e869272438023467ddd9156bfff0dfce08cf
Ilya <ilnikride@gmail.com>
2020-02-23 22:27:08 +0400

    refactoring Controller.kt

--- src/main/java/gui/Controller.kt
+++ src/main/java/gui/Controller.kt
@@ -34,32 +34,32 @@ class Controller {
     fun buttonClick(actionEvent: ActionEvent?) { // выполняем проверку, если хорошо то передаем на конвертацию текста
         if (checkField()) {
             val unparsedData = arrayOf(
-                textMsisdn!!.text,
-                textMsisdnB!!.text,
-                dateStarDate!!.value.format(DateTimeFormatter.ofPattern("dd.MM.YYYY")),
-                textStartTime!!.text,
-                textDuration!!.text, chPlusMinute!!.isSelected.toString(), chUrgency!!.isSelected.toString()
+                textMsisdn.text,
+                textMsisdnB.text,
+                dateStarDate.value.format(DateTimeFormatter.ofPattern("dd.MM.YYYY")),
+                textStartTime.text,
+                textDuration.text, chPlusMinute.isSelected.toString(), chUrgency.isSelected.toString()
             )
             val convert = Converter()
-            convert.convert(unparsedData, welcomeLabel!!)
+            convert.convert(unparsedData, welcomeLabel)
         }
     }
 
     // ограничиваем ввод букв в некоторые поля
     fun replaceDigit() {
-        replaceDigit(textMsisdn!!)
-        replaceDigit(textMsisdnB!!)
-        replaceDigit(textDuration!!)
+        replaceDigit(textMsisdn)
+        replaceDigit(textMsisdnB)
+        replaceDigit(textDuration)
     }
 
     // вызов проверки правильности введеной информации
     private fun checkField(): Boolean {
         val error =
-            checkField(textMsisdn!!, textMsisdnB!!, dateStarDate!!, textStartTime!!, textDuration!!)
+            checkField(textMsisdn, textMsisdnB, dateStarDate, textStartTime, textDuration)
         return if (error === InputError.NO_ERROR) {
             true
         } else { // вывод ошибки
-            welcomeLabel!!.text = error.textError
+            welcomeLabel.text = error.textError
             false
         }
     }

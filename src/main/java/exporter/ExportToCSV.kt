f9a16acd23c4a56af31298a78af05f5a8ce95978
live-HOCOK <zaordy@gmail.com>
2020-03-08 00:26:44 +0300

    task13 Remake form * переписал отображение ошибок ввода, теперь отобржаются как подсказка к кнопке * переписал CheckInput.kt для нового отображения * переписал Controller.kt для нового отображения * сломал CheckInputTest.kt

--- src/main/java/exporter/ExportToCSV.kt
+++ src/main/java/exporter/ExportToCSV.kt
@@ -4,23 +4,30 @@ import javafx.stage.DirectoryChooser
 import javafx.stage.Stage
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVPrinter
+import java.beans.EventHandler
 import java.io.BufferedWriter
+import java.lang.reflect.InvocationTargetException
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ExportToCSV {
+    var operator: String = ""
 
-    fun createCSV(data: String, operator: String) {
+    fun createCSV(data: String) {
         val dateFormatter = SimpleDateFormat("ddMMYYYY_HHmm")
         val fileName = "${operator}_${dateFormatter.format(Calendar.getInstance().time)}.csv"
-        val filePath = "${showChooserDirectory()}/$fileName"
-        val writer: BufferedWriter = Files.newBufferedWriter(Paths.get(filePath))
-        val csvPrinter = CSVPrinter(writer, CSVFormat.DEFAULT)
-        csvPrinter.printRecord(data)
-        csvPrinter.flush()
-        csvPrinter.close()
+        try {
+            val directory = showChooserDirectory()
+            val filePath = "$directory/$fileName"
+            val writer: BufferedWriter = Files.newBufferedWriter(Paths.get(filePath))
+            val csvPrinter = CSVPrinter(writer, CSVFormat.DEFAULT)
+            csvPrinter.printRecord(data)
+            csvPrinter.flush()
+            csvPrinter.close()
+        }
+        catch (e: IllegalStateException){ }
     }
 
     private fun showChooserDirectory(): String {
